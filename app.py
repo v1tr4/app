9cd2418329b92beb32342a52c23768e0a475acbd
v1tr4 <fitra.hidiyanto@gmail.com>
2023-03-22 22:25:45 +0700

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,10 @@ st.header('ini bagian headernya')
 # Subheader
 st.subheader(" Ini bagian subheader ")
 # Markdown
+st.markdown(' # markdown1 ')
+st.markdown(' ## markdown2 ')
+st.markdown(' ### markdown3 ')
+st.markdown(' ##### markdown4 ')
 # Caption
 # Code block
 # Text
