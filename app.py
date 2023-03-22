f12d3e56fc321356348c5d2aecea464cb46ee30a
v1tr4 <fitra.hidiyanto@gmail.com>
2023-03-22 22:23:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
 import streamlit as st
 
+#Text elements:
 
+# Title
 st.title('Streamlit app 1st attempt')
+# Header
+st.header('ini bagian headernya')
+# Subheader
+st.subheader(" Ini bagian subheader ")
+# Markdown
+# Caption
+# Code block
+# Text
+# LaTeX
+
+
