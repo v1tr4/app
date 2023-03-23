94e099b5f8e4e68bc2c27879cacf5cac2a3daec1
v1tr4 <fitra.hidiyanto@gmail.com>
2023-03-23 16:03:57 +0700

    Update app.py

--- app.py
+++ app.py
@@ -194,3 +194,19 @@ day = st.date_input(
      "When is your birthday?",
      datetime.date(2022, 7, 6))
 st.write('Your birthday is:', day)
+
+
+#Multi select
+selections = st.multiselect("Purchase", ["oranges", "apples", "bananas"])
+#Number input
+choice = st.number_input("Choose a number", 0, 50)
+#Text area
+text = st.text_area("Start typing")
+#Time input
+time = st.time_input("Dinner time")
+#file upload
+data = st.file_uploader("Share excel file")
+#Slider
+num = st.slider("Pick a number", 0, 5)
+#Select slider
+fruit = st.select_slider("Pick a fruit", ["Apple", "Orange", "Berries"])
